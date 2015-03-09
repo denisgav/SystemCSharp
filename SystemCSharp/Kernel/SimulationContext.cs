12bc5b8b7a1d2fe65d184db2e2dadc3c0a929bca
denisgav <gavden@gmail.com>
2015-03-09 16:35:12 +0200

    Added Simulation Object and Object Manager

--- SystemCSharp/Kernel/SimulationContext.cs
+++ SystemCSharp/Kernel/SimulationContext.cs
@@ -7,10 +7,13 @@ namespace SystemCSharp.Kernel
 {
     public class SimulationContext
     {
+        public const char HIERARCHY_CHAR = '.';
+
         private SimulationTimeParameters timeParameters;
         public SimulationTimeParameters TimeParameters
         {
             get { return timeParameters; }
+            set { timeParameters = value; }
         }
 
         private static SimulationContext globalSimContext;
@@ -30,6 +33,42 @@ namespace SystemCSharp.Kernel
             get { return isRunning; }
             set { isRunning = value; }
         }
+
+        private List<SimulationObject> childObjects;
+        public virtual List<SimulationObject> ChildObjects
+        {
+            get { return childObjects; }
+            set { childObjects = value; }
+        }
+
+        private List<SimulationEvent> childEvents;
+        public virtual List<SimulationEvent> ChildEvents
+        {
+            get { return childEvents; }
+            set { childEvents = value; }
+        }
+
+        private SimulationObjectManager simObjectManager;
+        public SimulationObjectManager SimObjectManager
+        {
+            get { return simObjectManager; }
+            set { simObjectManager = value; }
+        }
+
+        public SimulationObject GetActiveObject()
+        {
+            return simObjectManager.HierarchyCurr();
+        }
+
+        public SimulationContext()
+        {
+            simObjectManager = new SimulationObjectManager();
+            childObjects = new List<SimulationObject>();
+            childEvents = new List<SimulationEvent>();
+            isRunning = false;
+            timeParameters = new SimulationTimeParameters();
+        }
+        
         
         
     }
