5cdb8970a8a664460622ec68e317c8fd47065fa9
denisgav <gavden@gmail.com>
2015-05-24 16:47:47 +0300

    Added comments to all files.

--- SystemCSharp/kernel/sc_ver.cs
+++ SystemCSharp/kernel/sc_ver.cs
@@ -1,3 +1,21 @@
+/*****************************************************************************
+
+  The following code is derived, directly or indirectly, from the SystemC
+  source code Copyright (c) 1996-2014 by all Contributors.
+  All Rights reserved.
+
+  The contents of this file are subject to the restrictions and limitations
+  set forth in the SystemC Open Source License (the "License");
+  You may not use this file except in compliance with such restrictions and
+  limitations. You may obtain instructions on how to receive a copy of the
+  License at http://www.accellera.org/. Software distributed by Contributors
+  under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
+  ANY KIND, either express or implied. See the License for the specific
+  language governing rights and limitations under the License.
+
+ *****************************************************************************/
+
+
 public static class GlobalMembersSc_ver
 {
 
@@ -7,11 +25,11 @@ public static class GlobalMembersSc_ver
     }
     public static string sc_release()
     {
-        return "1.0";
+        return "0.0";
     }
     public static string sc_version()
     {
-        return "1.0";
+        return "0.0";
     }
 
 }
